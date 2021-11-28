664310d1f961384168c6de9a066e0ad4079d57ae
ASK259 <antti.kuusisto@edu.keuda.fi>
2021-11-29 00:25:12 +0200

    js päivitys

--- nelilaskin/koodi.js
+++ nelilaskin/koodi.js
@@ -2,23 +2,35 @@ let lu = "";
 let nu = "";
 let tau = ["+", "-", "*", "/", ",", "%"];
 let ind = 0;
+let ind2;
+let lu2 = [];
 
 let lisaa = (lu) => {nu += lu; document.getElementById("tulos").innerText = nu;}
 
 function laske() {
-
+  if (nu.search("%") > 0) {
+    lu2 = nu.split("%");
+    nu = lu2[0]*lu2[1]/100;
+    document.getElementById("tulos").innerText = nu;
+  } else {
+    document.getElementById("tulos").innerText = eval(nu);
+    nu = eval(nu);
+  }
 }
 
-let tyhjennys = (nu) => {nu = ""; document.getElementById("tulos").innerText = nu;}
+let tyhjennys = () => {nu = ""; document.getElementById("tulos").innerText = nu;}
 
-function kumitus() {
+function kumitus(lisaa) {
+  console.log(nu, typeof nu);
   for (let i = 0; i < nu.lenght; i++) {
     for (let j = 0; j < tau.lenght; j++) {
+      console.log(nu,tau);
       if (nu[i] == tau[j]) {
         ind = 0;
       }
     }
   }
-  nu = nu.substring(0, ind);
+  nu = nu.subString(0, ind);
+  console.log(nu);
   document.getElementById("tulos").innerText = nu;
 }
