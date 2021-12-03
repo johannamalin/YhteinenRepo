8eb0cbbe565fddd993b04d8169c01923d2ba23a2
ASK259 <antti.kuusisto@edu.keuda.fi>
2021-12-03 20:19:40 +0200

    Footer ei hyvä. js luo ja resetoi taulukon jossa harjoitus kuva nyt.

--- projekti_5/html_ideoiden_visualisointi.js
+++ projekti_5/html_ideoiden_visualisointi.js
@@ -1,9 +1,16 @@
 function luoPeli() {
-  let al = document.getElementById("peli");
+  let po = document.getElementById("peli");
+  console.log(po);
+  while (po.hasChildNodes()) {
+    po.removeChild(po.firstChild);
+  }
+  let test = [9,3,4,5,2,4];
+  //let al = document.getElementById("peli");
   let ko = document.getElementById("pelivalikko");
   let tau = Array.from(ko.options);
   let rM;
   let cM;
+  let gRII = (min, max) => {min = Math.ceil(min);max = Math.floor(max);return Math.floor(Math.random() * (max - min + 1) + min);}
   for (let i = 0;i<tau.length;i++) {
     if(tau[i].selected == true) {
       rM = parseInt(tau[i].value), cM = parseInt(tau[i].value.slice(-1));
@@ -11,15 +18,17 @@ function luoPeli() {
     }
   }
   console.log(tau);
-  console.log(typeof rM, typeof cM);
   let alk  = document.getElementById("peli");
   let tbl = document.createElement("table");
+  console.log(tbl);
   let tBo = document.createElement("tbody");
   for (let i = 0; i < rM; i++) {
     let row = document.createElement("tr");
     for (let j = 0; j < cM; j++) {
       let cll = document.createElement("td");
-      let clTe = document.createTextNode("row" + i +",cell"+ j);
+      let clTe = document.createTextNode("");
+      cll.setAttribute("tun", gRII(1, cM * cM));
+      console.log(cll);
       cll.appendChild(clTe);
       row.appendChild(cll);
     }
@@ -32,11 +41,20 @@ function luoPeli() {
 
 function piTe() {
   document.getElementById("peO").hidden = true;
-  document.getElementById("ai").hidden = false;
-  document.getElementById("pi").hidden = false;
+  /*document.getElementById("ai").hidden = false;
+  document.getElementById("pi").hidden = false;*/
 }
 
 function piilota() {
-  document.getElementById("ai").hidden = true;
-  document.getElementById("pi").hidden = true;
+  /*document.getElementById("ai").hidden = true;
+  document.getElementById("pi").hidden = true;*/
+}
+
+function nollaa() {
+  let po = document.getElementById("peli");
+  console.log(po);
+  while (po.hasChildNodes()) {
+    po.removeChild(po.firstChild);
+  }
+  document.getElementById("peO").hidden = false;
 }
