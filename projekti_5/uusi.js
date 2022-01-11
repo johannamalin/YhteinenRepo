a8324b6ec69852fbc4eee66843493d2f64693acc
ASK259 <antti.kuusisto@edu.keuda.fi>
2022-01-11 14:46:58 +0200

    Jotain edistystä. Toimii edelleen huonosti.

--- projekti_5/uusi.js
+++ projekti_5/uusi.js
@@ -64,7 +64,7 @@ let ai = 0;
 
 function kaKo() {
   let ko = document.getElementById("pelivalikko");
-  let tau = Array.from(ko.options); //luo taulukon pudotusvalikon tiedoista
+  let tau = Array.from(ko.options);
   let rM;
   let cM;
   for (let i = 0;i<tau.length;i++) {
@@ -79,34 +79,18 @@ function kaKo() {
 }
 
 function luoPeli() {
-  // muuttuja ja funktio nollaavat pelin
   let po = document.getElementById("peli");
   while (po.hasChildNodes()) {
     po.removeChild(po.firstChild);
   }
-  /*let ko = document.getElementById("pelivalikko");//hakee pudotusvalikon valinnan
-  let tau = Array.from(ko.options); //luo taulukon pudotusvalikon tiedoista
-  let rM;
-  let cM;
-  for (let i = 0;i<tau.length;i++) {
-    if(tau[i].selected == true) {
-      rM = parseInt(tau[i].value), cM = parseInt(tau[i].value.slice(-1));
-    }
-  }
-  let ma = rM * cM;
-  let kayKo = kortit.slice(0, ma);
-  sek(kayKo);*/
-  //console.log();
   let pAl = document.getElementById('peli');
   let di1 = document.createElement("div");
   di1.setAttribute("class", "pohja");
-  //let kuv = document.createElement("img");
   let kayKo = kaKo();
-  //console.log(kayKo);
   kayKo.forEach((arr, ind) => {
     let kuv1 = document.createElement('img');
     kuv1.src = "tummakansi.png";
-    kuv1.setAttribute("class", kayKo[ind].nimi);
+    kuv1.setAttribute("data-id", kayKo[ind].nimi);
     kuv1.setAttribute("name", kayKo[ind].img);
     kuv1.addEventListener("click", kaanna);
     di1.appendChild(kuv1);
@@ -119,81 +103,86 @@ function luoPeli() {
   aiTu.appendChild(tuTe);
   pAl.appendChild(aiTu);
 }
-//let ni = document.querySelector("img").name;
-//let tu = document.querySelector("img").className;
-function kaanna() {
-  console.log();
-  let ni = document.querySelector("img").name;
-  console.log(ni);
-  let nim;
-  console.log(ni.name);
-  let tu = document.querySelector("img").className;
+
+let ni = document.querySelector("img").name;
+//let tu = document.querySelector("img").class;
+
+function kaanna(ni) {
+  //console.log(typeof ni);
   //console.log(ni);
-  //let va = this.dataset.id;
-  //koVa.push(kayKo[va].nimi);
-  koVa.push(ni);
-  //koTu.push(va);
-  koTu.push(tu);
-  console.log(koVa);
-  let ap = 0;
-  //console.log(koTu);
+  let nim = ni.target;
+  //console.log(nim);
+  let kuTi  = Array.from(nim.attributes);
+  //console.log(kuTi);
+  let apu;
+  let apu1;
+  apu = kuTi.slice(2);
+  apu1 = kuTi.slice(1);
+  let tie = apu[0];
+  let tie1 = apu1[0]
+  //console.log(tie);
+  //console.log(typeof tie);
+  let kuNi = tie.nodeValue;
+  let kuTu = tie1.nodeValue;
+  //console.log(kuNi);
+  //console.log(kuTu);
+  koVa.push(kuNi);
+  koTu.push(kuTu);
   this.classList.add("flip");
-  //console.log(kortit);
   let koKu = kortit.filter(obj => obj.img == ni);
-  let oKu = koKu[0].img;
-  console.log(oKu);
-  this.setAttribute("src", ni);
-  ap++
+  this.setAttribute("src", kuNi);
   if (koVa.length == 2) {
-    setTimeout(tar(koVa,koTu), 1000);
-    ap=0;
+    setTimeout(tar(koVa,koTu,ni), 1000);
   } else {
-    setTimeout(function(ni) {
+    setTimeout(function(ni1) {
       ni = document.querySelector("img");
       ni.classList.add("flip2");
       ni.setAttribute("src", "tummakansi.png");
-      ni = "";
-      //console.log(ni);
       }, 1000);
   }
-  ni = "";
-  nim = "";
-  console.log(ni);
+  console.log(koVa);
+  console.log(pi);
+  return;
 }
-
-function tar() {
-  //console.log(koVa);
-  //console.log(koTu);
+console.log(pi);
+function tar(ni) {
+  console.log(ni);
   let kuv = document.querySelectorAll("img");
-  let pi = 0;
-  //let ekaTu = koTu[0];
-  //console.log(ekaTu);
-  //let tokaTu = koTu[1];
-  let ekaNa = koVa[0];
-  let tokaNa = koVa[1];
-  if (ekaNa == tokaNa){
+  console.log(kuv);
+  let kuTi = Array.from(kuv);
+  console.log(kuTi);
+  console.log(kuv);
+  //console.log(kuv[koVa[0]]);
+  let ekaNa = kuTi.filter(obj => obj.name == koVa[0]);
+  console.log(ekaNa);
+  let ekaKu = ekaNa[0];
+  let tokaNa = kuTi.filter(obj => obj.name == koVa[1]);
+  let tokaKu = tokaNa[0];
+  console.log(tokaKu);
+  let ekaTu = koTu[0];
+  console.log(ekaTu);
+  let tokaTu = koTu[1];
+  console.log(tokaTu);
+  if (ekaTu == tokaTu){
+    ekaKu.setAttribute("src", ni[0]);
     pi++;
     console.log(pi);
     koVa = [];
-    koTu = []
+    koTu = [];
   } else {
-    kuv[ekaNa].setAttribute("src", "tummakansi.png");
-    kuv[tokaNa].setAttribute("src", "tummakansi.png");
+    tokaKu.classList.add("flip2");
+    tokaKu.setAttribute("src", "tummakansi.png");
     koVa = [];
-    //koTu = [];
+    koTu = [];
+    console.log(tokaKu);
   }
-  ni = "";
-  koVa = [];
-  koKu = [];
-  oKu = "";
-  koTu = []
-  console.log(ni);
+  return ni,koVa,koTu, pi;
 }
 
 function sek(kor) {
   kor.sort(() => 0.5 - Math.random());
 }
-
+console.log(pi);
 function respo() {
   let ti = window.innerWidth;
   let tu = document.getElementById("vika");
